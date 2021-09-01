7c28c39d6d51fdd6da0e2804972734a7487e06c8
ybaha <58085088+ybaha@users.noreply.github.com>
2021-09-01 04:12:48 +0300

    updated urls for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,15 @@ import { router } from "./routes";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
-mongoose.connect("mongodb://localhost/istekram");
+const mongooseURL =
+  process.env.NODE_ENV === "prod"
+    ? "mongodb+srv://baha:<password>@cluster0.x0mow.mongodb.net/myFirstDatabase"
+    : "mongodb://localhost/istekram";
+
+mongoose.connect(mongooseURL);
 
 app.use(cors());
 app.use(json());
